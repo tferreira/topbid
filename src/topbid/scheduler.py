f0c4f9a60fd6d88185a7ca8e278574fbefd91f58
ruth <fulura@gmail.com>
2023-04-02 21:26:13 +0900

    catch & log thread exceptions

--- src/topbid/scheduler.py
+++ src/topbid/scheduler.py
@@ -1,8 +1,15 @@
 """ Scheduler """
 
+import logging
 import threading
 import time
 from collections.abc import Callable
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+)
+logger = logging.getLogger("topbid_scheduler")
 
 
 class RepeatEvery(threading.Thread):
@@ -21,7 +28,10 @@ class RepeatEvery(threading.Thread):
         while self.runable:
             # Sleeping beforehand, letting time for dependencies to startup
             time.sleep(self.interval)
-            self.func(*self.args, **self.kwargs)
+            try:
+                self.func(*self.args, **self.kwargs)
+            except Exception as exc:  # pylint: disable=broad-except
+                logger.exception(exc)
 
     def stop(self):
         """Stops the scheduler"""
